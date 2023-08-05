9a2652c913252ee84714cc340959b4e45f2e0e23
samrat53 <samrat05hccs@gmail.com>
2023-08-06 04:45:08 +0530

    completed dynamic name render

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./form";
 
 function App() {
+  let [name, setName] = useState("");
+  const currDate = new Date();
+  // let birthday = new Date();
+  let [date, setDate] = useState(currDate);
+  let [birthday, setBirthday] = useState(new Date());
+  let temp: Date;
+  // const showAlert=(birthday, currDate)=>{
+  //   console.log(birthday);
+  //   console.log("hmm")
+  //   console.log(currDate);
+  //   return
+  // }
   return (
     <>
-      <Form></Form>
+      <Form
+        heading={"Hemlo " + name}
+        handleTextChange={(event1) => setName(event1.target.value)}
+        handleDateEntry={(event2) => {
+          setDate(event2.target.value);
+          temp = date;
+        }}
+        // showMessage={() => setBirthday(temp)}
+        showMessage={showAlert}
+      ></Form>
     </>
   );
 }
--- src/form.tsx
+++ src/form.tsx
@@ -1,10 +1,19 @@
-const Form = () => {
+import { ChangeEvent } from "react";
+
+interface formProps{
+    heading: string;
+    handleTextChange: ((event1: ChangeEvent<HTMLInputElement>)=>void);
+    handleDateEntry: (event2: ChangeEvent)=>void;
+    showMessage: ()=>void;
+}
+
+const Form = ({heading, handleTextChange,handleDateEntry,showMessage} : formProps) => {
   return (
     <div className="container">
-      <h1 className="heading">Hello{}</h1>
-      <input type="text" className="name" placeholder="Enter Name" />
-      <input type="date" className="birthday" />
-      <button className="submit">Submit</button>
+      <h1 className="heading">{heading}</h1>
+      <input type="text" className="name" placeholder="Enter Name" onChange={handleTextChange} />
+      <input type="date" className="birthday" onChange={handleDateEntry}/>
+      <button className="submit" onClick={showMessage}>Submit</button>
     </div>
   );
 };
