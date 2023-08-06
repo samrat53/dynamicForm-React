58247ae80efe10e35bc684a32aa142a7c66cd956
samrat53 <samrat05hccs@gmail.com>
2023-08-07 02:36:36 +0530

    adding more functionnality

--- src/form.tsx
+++ src/form.tsx
@@ -3,8 +3,28 @@ import { ChangeEvent, useState } from "react";
 const Form = () => {
   let [mouseOver, setMouseOver] = useState(false);
   let [name, setName] = useState("");
+
+  let currdate = new Date().getDate();
+  let [dob, setDob] = useState(currdate.toString());
+
   const handleTextChange = (event1: ChangeEvent<HTMLInputElement>) =>
     setName(event1.target.value);
+
+  const handleDateEntry = (event2: ChangeEvent<HTMLInputElement>) => {
+    const birthdayString = event2.target.value.toString();
+    console.log(`birthdayString: ${birthdayString} and type: ${typeof birthdayString}`);
+
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+    const todayDateString = `${year}-${month}-${day}`; //string format of today's date
+    console.log(`todayDateString: ${todayDateString} and type: ${typeof todayDateString}`);
+
+    console.log(Math.abs(birthdayString - todayDateString)); //issue: logging NaN in console
+
+    return setDob(birthdayString);
+  };
 
   return (
     <div className="container">
@@ -12,9 +32,18 @@ const Form = () => {
       <input
         type="text"
         className="name"
+        name="goodName"
         placeholder="Enter Name"
         value={name}
         onChange={handleTextChange}
+      />
+
+      <input
+        type="date"
+        className="name dateEnter"
+        name="dob"
+        value={dob}
+        onChange={handleDateEntry}
       />
 
       <button
