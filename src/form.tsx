b1abce492e9fa69ae05c109439d95c181ff958e8
samrat53 <samrat05hccs@gmail.com>
2023-08-08 01:37:06 +0530

    prefinal commit

--- src/form.tsx
+++ src/form.tsx
@@ -3,13 +3,10 @@ import { ChangeEvent, useState } from "react";
 const Form = () => {
   let [mouseOver, setMouseOver] = useState(false);
   let [name, setName] = useState("");
+  let [dob, setDob] = useState(new Date().toDateString());
 
-  let currdate = new Date().getDate();
-  let [dob, setDob] = useState(currdate.toString());
-
-
-  let globalName=name;
-  let [globalDaysLeft,setGlobalDays]=useState(-1);
+  let globalName = name;
+  let [globalDaysLeft, setGlobalDays] = useState(-1);
 
   const handleTextChange = (event1: ChangeEvent<HTMLInputElement>) =>
     setName(event1.target.value);
@@ -20,31 +17,21 @@ const Form = () => {
       `birthdayString: ${birthdayString} and type: ${typeof birthdayString}`
     );
 
-    // const today = new Date();
-    // const year = today.getFullYear();
-    // const month = String(today.getMonth() + 1).padStart(2, "0");
-    // const day = String(today.getDate()).padStart(2, "0");
-    // const todayDateString = `${year}-${month}-${day}`; //string format of today's date
-    // console.log(
-    //   `todayDateString: ${todayDateString} and type: ${typeof todayDateString}`
-    // );
-
     const today = new Date();
     const [year, month, day] = birthdayString.split("-").map(Number);
     const birthday = new Date(year, month - 1, day);
     const timeDiff = birthday.getTime() - today.getTime();
     const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-    globalDaysLeft=daysLeft;
+
+    globalDaysLeft = daysLeft;
     setGlobalDays(daysLeft);
 
     return setDob(birthdayString);
   };
 
-
-
   return (
     <div className="container">
-      <h1 className="heading">{"Voila! " + name}</h1>
+      <h1 className="heading">{"Voila! "+name}</h1>
       <input
         type="text"
         className="name"
@@ -61,13 +48,20 @@ const Form = () => {
         value={dob}
         onChange={handleDateEntry}
       />
+      <h1 className="heading subHeading">Enter your next birthday date and submit</h1>
 
       <button
         className="submit"
         name="alertButton"
         onMouseOver={() => setMouseOver(true)}
         onMouseOut={() => setMouseOver(false)}
-        onClick={()=>alert(`${globalName} : days left for your birthday is: ${globalDaysLeft}`)}
+        onClick={() =>
+          globalDaysLeft === 0
+            ? alert(`Hey ${globalName}, Happy Birthday. Enjoy its your day`)
+            : alert(
+                `The developer wishes ${globalName} Happy Birthday in ${globalDaysLeft} days advance`
+              )
+        }
         style={{
           backgroundColor: mouseOver
             ? "rgb(84, 249, 255)"
